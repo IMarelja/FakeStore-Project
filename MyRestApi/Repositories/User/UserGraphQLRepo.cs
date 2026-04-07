1df02c784ae9bb916a3c93cdff05d96a59dce760
IMarelja <imarelja@algebra.hr>
2026-04-08 01:26:09 +0200

    User connection setup

--- MyRestApi/Repositories/User/UserGraphQLRepo.cs
+++ MyRestApi/Repositories/User/UserGraphQLRepo.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using FakeStore.Models;
 using FakeStore.ViewModel;
 using MyRestApi.Middleware;
@@ -6,15 +8,115 @@ namespace MyRestApi.Repositories;
 
 public class UserGraphQLRepo : IUserRepo
 {
-    public Task<List<User>> GetAllAsync() =>
-        throw new Exception("GetAll users is not yet implemented.");
+    private readonly HttpClient _http;
 
-    public Task<User?> GetByIdAsync(int userId) =>
-        throw new Exception("GetById user is not yet implemented.");
+    private static readonly JsonSerializerOptions _writeOptions = new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
 
-    public Task<User?> UpdateAsync(int userId, UserUpdate req) =>
-        throw new Exception("UpdateUser is not yet implemented.");
+    private static readonly JsonSerializerOptions _readOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
 
-    public Task<bool> DeleteAsync(int userId) =>
-        throw new Exception("DeleteUser is not yet implemented.");
+    public UserGraphQLRepo(IHttpClientFactory factory)
+    {
+        _http = factory.CreateClient("graphql");
+    }
+    public async Task<List<User>> GetAllAsync()
+    {
+        const string query = """
+            {
+              users {
+                userId
+                username
+                email
+                password
+                role
+              }
+            }
+            """;
+
+        var data = await SendAsync(query);
+        return JsonSerializer.Deserialize<List<User>>(
+            data.GetProperty("users").GetRawText(), _readOptions)!;
+    }
+
+    public async Task<User?> GetByIdAsync(int userId) {
+        const string query = """
+            query($id: Int!) {
+              user(id: $id) {
+                userId
+                username
+                email
+                password
+                role
+              }
+            }
+            """;
+
+        var data = await SendAsync(query, new { id = userId });
+        var userEl = data.GetProperty("user");
+
+        if (userEl.ValueKind == JsonValueKind.Null)
+            return null;
+
+        return JsonSerializer.Deserialize<User>(userEl.GetRawText(), _readOptions)!;
+    }
+
+    public async Task<User?> UpdateAsync(int userId, UserUpdate req) {
+        const string mutation = """
+            mutation($id: Int!, $input: UserInput!) {
+              updateUser(id: $id, input: $input) {
+                userId
+                username
+                email
+                password
+                role
+              }
+            }
+            """;
+
+        var data = await SendAsync(mutation, new
+        {
+            id = userId,
+            input = new
+            {
+                req.Username,
+                req.Email,
+                req.Password
+            }
+        });
+        return JsonSerializer.Deserialize<User>(
+            data.GetProperty("updateUser").GetRawText(), _readOptions)!;
+    }
+
+    public async Task<bool> DeleteAsync(int userId) {
+        const string query = """
+            mutation($id: Int!) {
+              deleteUser(id: $id)
+            }
+            """;
+
+        var data = await SendAsync(query, new { id = userId });
+        return data.GetProperty("deleteUser").GetBoolean();
+    }
+
+    private async Task<JsonElement> SendAsync(string query, object? variables = null)
+    {
+        try
+        {
+            var body = JsonSerializer.Serialize(new { query, variables }, _writeOptions);
+            var response = await _http.PostAsync("graphql", new StringContent(body, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonDocument.Parse(json).RootElement.GetProperty("data").Clone();
+        }
+        catch (HttpRequestException ex)
+        {
+            throw new GraphQLServiceException("GraphQL service is unavailable.", ex);
+        }
+    }
 }
