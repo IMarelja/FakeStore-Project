f892346279eb959c9be19423f339db6cd9f2b6a5
IMarelja <imarelja@algebra.hr>
2026-04-10 19:22:13 +0200

    Not returning the roles of the user

--- MyRestApi/Repositories/Authentication/AuthenticationGraphQLRepo.cs
+++ MyRestApi/Repositories/Authentication/AuthenticationGraphQLRepo.cs
@@ -34,6 +34,7 @@ public class AuthenticationGraphQLRepo : IAuthenticationRepo
                 username
                 email
                 password
+                role
               }
             }
             """;
@@ -54,6 +55,7 @@ public class AuthenticationGraphQLRepo : IAuthenticationRepo
                 username
                 email
                 password
+                role
               }
             }
             """;
