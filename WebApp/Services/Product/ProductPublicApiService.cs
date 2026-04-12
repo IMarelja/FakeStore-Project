9ce7b66e7708af5cf155f6c96568a181b0e55600
IMarelja <imarelja@algebra.hr>
2026-04-12 23:31:44 +0200

    Product public api

--- WebApp/Services/Product/ProductPublicApiService.cs
+++ WebApp/Services/Product/ProductPublicApiService.cs
@@ -4,17 +4,34 @@ namespace FakeStore.WebApp.Service;
 
 public class ProductPublicApiService : IProductService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ProductPublicApiService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IEnumerable<ProductRead>> GetAll()
+    {
+        var client = _httpClientFactory.CreateClient("ApiClient");
+        var response = await client.GetAsync("products");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException(
+                $"Get products failed ({(int)response.StatusCode} {response.ReasonPhrase}). {body}");
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<IEnumerable<ProductRead>>();
+        return result ?? [];
+    }
     public Task<ReviewProductRead> AddReview(ReviewProductCreate review)
     {
         throw new NotImplementedException();
     }
 
     public Task<ProductRead> CreateProduct(ProductCreate product)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<ProductRead>> GetAll()
     {
         throw new NotImplementedException();
     }
