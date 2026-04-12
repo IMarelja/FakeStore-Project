d4a18d0a375b3241a885260422380588b2246a0c
IMarelja <imarelja@algebra.hr>
2026-04-12 23:06:01 +0200

    Fetching users in Public api

--- WebApp/Services/User/UserPublicApiService.cs
+++ WebApp/Services/User/UserPublicApiService.cs
@@ -4,6 +4,29 @@ namespace FakeStore.WebApp.Service;
 
 public class UserPublicApiService : IUserService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public UserPublicApiService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IEnumerable<UserRead>> GetAll()
+    {
+        var client = _httpClientFactory.CreateClient("ApiClient");
+        var response = await client.GetAsync("user");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            throw new HttpRequestException(
+                $"Get users failed ({(int)response.StatusCode} {response.ReasonPhrase}). {body}");
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<IEnumerable<UserRead>>();
+        return result ?? [];
+    }
+
     public Task<bool> DeleteMe()
     {
         throw new NotImplementedException();
@@ -20,11 +43,6 @@ public class UserPublicApiService : IUserService
     }
 
     public Task<UserRead?> EditUser(int id, UserUpdate user)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<UserRead>> GetAll()
     {
         throw new NotImplementedException();
     }
