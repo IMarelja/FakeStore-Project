afb1162b5f0cf9c679877c32ce9c3de65382bd09
IMarelja <imarelja@algebra.hr>
2026-04-14 23:03:26 +0200

    useless variables

--- WebApp/Models/WeatherViewModel.cs
+++ WebApp/Models/WeatherViewModel.cs
@@ -6,17 +6,6 @@ public class WeatherViewModel
     public bool Searched { get; set; }
     public bool Found { get; set; }
     public List<WeatherCityViewModel> Cities { get; set; } = [];
-
-    public string City { get; set; } = "";
-    public string Temperature { get; set; } = "";
-    public string Humidity { get; set; } = "";
-    public string Pressure { get; set; } = "";
-    public string PressureTendency { get; set; } = "";
-    public string WindDirection { get; set; } = "";
-    public string WindSpeed { get; set; } = "";
-    public string Description { get; set; } = "";
-    public string Date { get; set; } = "";
-    public string Term { get; set; } = "";
 }
 
 public class WeatherCityViewModel
