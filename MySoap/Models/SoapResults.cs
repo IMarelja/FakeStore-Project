ba02817988b0d7466f112c9da0f73baf94d15533
IMarelja <imarelja@algebra.hr>
2026-04-14 18:42:53 +0200

    nicer

--- MySoap/Models/SoapResults.cs
+++ MySoap/Models/SoapResults.cs
@@ -5,18 +5,27 @@ namespace MySoap.Models;
 [DataContract]
 public class ProductSoap
 {
-    [DataMember] public int Id { get; set; }
-    [DataMember] public string Name { get; set; } = string.Empty;
-    [DataMember] public string Description { get; set; } = string.Empty;
-    [DataMember] public decimal Price { get; set; }
-    [DataMember] public string Category { get; set; } = string.Empty;
-    [DataMember] public string Brand { get; set; } = string.Empty;
-    [DataMember] public double Rating { get; set; }
+    [DataMember] 
+    public int Id { get; set; }
+    [DataMember] 
+    public string Name { get; set; } = string.Empty;
+    [DataMember] 
+    public string Description { get; set; } = string.Empty;
+    [DataMember] 
+    public decimal Price { get; set; }
+    [DataMember] 
+    public string Category { get; set; } = string.Empty;
+    [DataMember] 
+    public string Brand { get; set; } = string.Empty;
+    [DataMember] 
+    public double Rating { get; set; }
 }
 
 [DataContract]
 public class SearchResult
 {
-    [DataMember] public List<ProductSoap> Products { get; set; } = [];
-    [DataMember] public int Count { get; set; }
+    [DataMember] 
+    public List<ProductSoap> Products { get; set; } = [];
+    [DataMember] 
+    public int Count { get; set; }
 }
