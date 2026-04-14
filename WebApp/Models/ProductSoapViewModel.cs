e168f8a1480f5fcca9f98c8ee64faf6fcfc71e07
IMarelja <imarelja@algebra.hr>
2026-04-14 23:18:10 +0200

    range functionality

--- WebApp/Models/ProductSoapViewModel.cs
+++ WebApp/Models/ProductSoapViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using FakeStore.WebApp.SoapClients;
 
 namespace FakeStore.WebApp.Models;
@@ -5,7 +6,12 @@ namespace FakeStore.WebApp.Models;
 public class ProductSoapViewModel
 {
     public string searchedTerm { get; set; } = String.Empty;
+
+    [Range(0, double.MinValue, ErrorMessage = "Min price must be 0 or more.")]
     public double? selectedMinPrice { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "Min price must be 0 or more.")]
+    
     public double? selectedMaxPrice { get; set; }
     public bool found { get; set; }
     public string? errorMessage { get; set; }
